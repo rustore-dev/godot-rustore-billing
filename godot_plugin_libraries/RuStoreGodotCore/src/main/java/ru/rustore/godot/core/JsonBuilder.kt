95c2dac6461f4702b5c707fe8b569aa6e2701b69
rpelmegov <rpelmegov@ya.ru>
2024-03-12 10:56:34 +0300

    Update Core plugin

---
+++ godot_plugin_libraries/RuStoreGodotCore/src/main/java/ru/rustore/godot/core/JsonBuilder.kt
@@ -0,0 +1,21 @@
+package ru.rustore.godot.core
+
+import com.google.gson.Gson
+
+object JsonBuilder {
+    private val gson: Gson by lazy { Gson() }
+
+    fun toJson(throwable: Throwable?): String {
+        var json = gson.toJson(throwable)
+
+        throwable?.let {
+            if (json.isNotEmpty() && json.endsWith("}")) {
+                val splitter = if (json.contains(":")) "," else ""
+                val simpleName = """"simpleName":"${throwable::class.java.simpleName}""""
+                json = json.dropLast(1).plus(splitter).plus(simpleName).plus("}")
+            }
+        }
+
+        return json
+    }
+}
